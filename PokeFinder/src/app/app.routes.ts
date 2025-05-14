707f6a64d28ae667310f7ecc5aaecfbf3cc27023
bo french <bfrench3@emich.edu>
2025-05-14 13:56:30 -1000

    added html injecting safeguard using auth guarding

--- PokeFinder/src/app/app.routes.ts
+++ PokeFinder/src/app/app.routes.ts
@@ -6,13 +6,15 @@ import { WordleComponent } from './wordle/wordle.component';
 import { WhereamiComponent } from './whereami/whereami.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { AuthGuard } from '../auth.guard';
+
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent },
-    { path: 'starter', component: StarterComponent },
-    { path: 'trainers', component: TrainersComponent },
-    { path: 'wordle', component: WordleComponent },
-    { path: 'whereami', component: WhereamiComponent },
-    { path: 'login', component: LoginComponent },
-    { path: 'signup', component: SignupComponent }
+    { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'starter', component: StarterComponent, canActivate: [AuthGuard] },
+    { path: 'trainers', component: TrainersComponent, canActivate: [AuthGuard] },
+    { path: 'wordle', component: WordleComponent, canActivate: [AuthGuard] },
+    { path: 'whereami', component: WhereamiComponent, canActivate: [AuthGuard] },
+    { path: 'login', component: LoginComponent, },
+    { path: 'signup', component: SignupComponent, }
 ];
